441974c07d446896d0fae96d95951f3ef8caecf3
IRUL08 <mhdkhairulhatami08@gmail.com>
2025-05-28 16:15:41 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <AtributBindings/>
+  <DeclarativeRendering/>
+  <EventListeners />
+  <FormBindings/>
+  <ComputedProperty/>
+  <ConditionalRendering/>
+  <LifecycleTemplateRefs/>
+  <Watchers/>
+  <ListRendering/>
+
+  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AtributBindings from './components/icons/AtributBindings.vue';
+import EventListeners from './components/icons/EventListeners.vue';
+import DeclarativeRendering from './components/icons/DeclarativeRendering.vue';
+import FormBindings from './components/icons/FormBindings.vue';
+import ComputedProperty from './components/icons/ComputedProperty.vue';
+import ConditionalRendering from './components/icons/ConditionalRendering.vue';
+import LifecycleTemplateRefs from './components/icons/LifecycleTemplateRefs.vue';
+import ListRendering from './components/icons/ListRendering.vue';
+import Watchers from './components/icons/Watchers.vue';
+
+</script>
+
+<style scoped>
+
+</style>
